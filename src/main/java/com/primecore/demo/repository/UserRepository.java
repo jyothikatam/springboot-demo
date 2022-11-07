98c51e09d164cab8072b20b22a1eb8fa33e79b1e
jyothikatam <jyothikatam26@gmail.com>
2022-11-07 16:36:10 +0530

    added heroku sample postgres db porperties

--- src/main/java/com/primecore/demo/repository/UserRepository.java
+++ src/main/java/com/primecore/demo/repository/UserRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.repository.query.Param;
 import com.primecore.demo.entities.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-	@Query(value = "select * from user where user_id=:userId", nativeQuery = true)
+	@Query(value = "select * from users where user_id=:userId", nativeQuery = true)
 	User getById(@Param("userId") Long userId);
 
 }
